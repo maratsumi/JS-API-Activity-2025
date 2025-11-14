d52f334f9e0aa5f2ba4ca69f6697e1a798de655b
Gelo <90490718+maratsumi@users.noreply.github.com>
2025-11-14 17:52:56 +0800

    dictionary page

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const port = 3000;
 app.use(express.static("static"));
 
 app.get("/", (req, res) => {
-  res.status(200);
-  //   res.send("Hello World!");
   res.sendFile(__dirname + "/static/index.html");
 });
 
+app.get("/dictionary/:query", (req, res) => {
+  const DICTIONARY_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  search_query = DICTIONARY_URL + req.params.query;
+  console.log(search_query);
+  fetch(search_query)
+    .then((result) => {
+      return result.json();
+    })
+    .then((result) => {
+      res.end(JSON.stringify(result));
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}: http://127.0.0.1:3000`);
 });
